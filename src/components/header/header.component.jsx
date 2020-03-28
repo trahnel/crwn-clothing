c6ec20e421fde47fda6eb32f723f6a8977fc0fb4
Taavi Rahnel <taavi@spotify.com>
2020-03-28 16:36:14 +0100

    Redirect to home on already signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -20,7 +20,6 @@ const Header = ({ currentUser }) => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
-      {console.log(currentUser)}
       {currentUser ? (
         <div className="option" onClick={() => auth.signOut()}>
           SIGN OUT
