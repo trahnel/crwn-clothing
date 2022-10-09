452216b5a91a4f25c9111fe4520460834873d847
Taavi <rahnel.taavi@gmail.com>
2022-10-09 12:06:50 +0200

    Fix app test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './redux/store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
